657bb17d995d80d1e96f4e4b638ab3d4c8e58481
nasnimal <nasnimal@gmail.com>
2016-12-03 12:54:00 +0100

    Styles

--- web/tinymap.mapstyle.js
+++ web/tinymap.mapstyle.js
@@ -1,35 +1,35 @@
 /*leaflet style*/
 
 var green = L.icon({
-    iconUrl: 'icons/pin-green.png',
+    iconUrl: 'icons/pin-green.svg',
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
     popupAnchor:  [1, 1] // point from which the popup should open relative to the iconAnchor
 });
 
 var yellow = L.icon({
-    iconUrl: 'icons/pin-yellow.png',
+    iconUrl: 'icons/pin-yellow.svg',
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
     popupAnchor:  [1, 1] // point from which the popup should open relative to the iconAnchor
 });
 
 var orange = L.icon({
-    iconUrl: 'icons/pin-orange.png',
+    iconUrl: 'icons/pin-orange.svg',
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
     popupAnchor:  [1, 1] // point from which the popup should open relative to the iconAnchor
 });
 
 var red = L.icon({
-    iconUrl: 'icons/pin-red.png',
+    iconUrl: 'icons/pin-red.svg',
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
     popupAnchor:  [1, 1] // point from which the popup should open relative to the iconAnchor
 });
 
 var gray = L.icon({
-    iconUrl: 'icons/pin-gray.png',
+    iconUrl: 'icons/pin-gray.svg',
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
     popupAnchor:  [1, 1] // point from which the popup should open relative to the iconAnchor
@@ -37,11 +37,11 @@ var gray = L.icon({
 
 var pointToLayer =  function(feature, latlng){
         switch (feature.properties.state) {
-            case 'здоровое':    return L.marker(latlng, {icon: green});
-            case 'ослабленное':    return L.marker(latlng, {icon: yellow});
-            case 'сильно ослабленное':    return L.marker(latlng, {icon: orange});
-            case 'отмирающее':    return L.marker(latlng, {icon: red});
-            case 'сухостой':    return L.marker(latlng, {icon: gray});
+            case 'Здоровое':    return L.marker(latlng, {icon: green});
+            case 'Ослабленное':    return L.marker(latlng, {icon: yellow});
+            case 'Сильно ослабленное':    return L.marker(latlng, {icon: orange});
+            case 'Отмирающее':    return L.marker(latlng, {icon: red});
+            case 'Сухостой':    return L.marker(latlng, {icon: gray});
             default:        return L.marker(latlng, {icon: green});
         }
 }
