6e52b25d9ac4353142e5b2eccf859be24ce5ffd9
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-12-03 12:55:01 +0100

    Add onCreate to Main Activity

--- android.tree_mapper/app/src/main/java/com/nextgis/woody/activity/MainActivity.java
+++ android.tree_mapper/app/src/main/java/com/nextgis/woody/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nextgis.woody.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!mPreferences.getBoolean(SettingsConstants.KEY_PREF_INTRO, false)) {
+            startActivity(new Intent(this, IntroActivity.class));
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.activity_main_no_permissions);
+        setToolbar(R.id.main_toolbar);
+        setTitle(getText(R.string.app_name));
+
+        mCurrentViewState = savedInstanceState != null ? savedInstanceState.getInt(KEY_CURRENT_VIEW) : -1;
+        mCurrentTab = savedInstanceState != null ? savedInstanceState.getInt(KEY_CURRENT_TAB, 1) : 1;
+        findViewById(R.id.grant_permissions).setOnClickListener(this);
+
+        if (!hasPermissions())
+            requestPermissions();
+        else
+            start();
     }
 }
